a292ae21aca22d66443f5b60fcb3f031f06ed90a
Brahmi Gangwar <brahmig2006@gmail.com>
2026-06-29 18:28:12 +0530

    Move Firebase config to environment variables

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -8,14 +8,13 @@ import {
 } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBsA8DYTofbQecBhPZEd3NDOhViWBrwkvE",
-    authDomain: "community-hero-ai-brahmi.firebaseapp.com",
-    projectId: "community-hero-ai-brahmi",
-    storageBucket: "community-hero-ai-brahmi.firebasestorage.app",
-    messagingSenderId: "510336893368",
-    appId: "1:510336893368:web:c17829ee4179ee8f038f4d",
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
 };
-
 const app =
     getApps().length
         ? getApps()[0]
