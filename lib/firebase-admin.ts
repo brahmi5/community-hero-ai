371127f5950afa7c275e2ad46933ddce0ca26a1a
Brahmi Gangwar <brahmig2006@gmail.com>
2026-06-30 11:26:22 +0530

    Use environment variables for Firebase Admin

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -1,13 +1,15 @@
 import { cert, getApps, initializeApp } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 
-import serviceAccount from "../serviceAccount/firebase-key.json";
-
 const app =
     getApps().length > 0
         ? getApps()[0]
         : initializeApp({
-            credential: cert(serviceAccount as any),
+            credential: cert({
+                projectId: process.env.FIREBASE_PROJECT_ID,
+                clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+                privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+            }),
         });
 
 export const adminDb = getFirestore(app);
